fc3ab8838f50d44f8e1deebcc9be6ffdc479bf68
Samuel Agyemang <89325057+Sam-Tech-Ideas@users.noreply.github.com>
2022-07-29 23:34:20 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,6 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <header className="App-headsetCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
           <a
          res.html"
             target="_blank"
