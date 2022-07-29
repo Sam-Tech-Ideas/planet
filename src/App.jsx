f2afe41b686007c30c53a2184faf5b2b43be8aef
Samuel Agyemang <89325057+Sam-Tech-Ideas@users.noreply.github.com>
2022-07-29 23:37:06 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,7 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-          <a
-         res.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+        
   )
 }
 
